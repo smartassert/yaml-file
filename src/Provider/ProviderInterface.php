8c9c60d585aa632a254d73f4e6047512f1ed9a9b
Jon Cram <webignition@gmail.com>
2022-03-11 18:14:49 +0000

    Add base exception to be thrown by ProviderInterface::provide()

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace SmartAssert\YamlFile\Provider;
 
+use SmartAssert\YamlFile\Exception\ProvisionException;
 use SmartAssert\YamlFile\Model\YamlFile;
 
 interface ProviderInterface
 {
     /**
+     * @throws ProvisionException
+     *
      * @return \Generator<YamlFile>
      */
     public function provide(): \Generator;
