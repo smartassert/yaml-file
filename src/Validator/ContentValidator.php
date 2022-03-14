bd49606673da3bc0174013167e2e7ef69579128b
Jon Cram <webignition@gmail.com>
2022-03-14 14:39:05 +0000

    Revert "Add ValidatorInterface (#25)" (#26)
    This reverts commit cac20b2f097d6c1822ef72650e7816f52f26b625.

--- src/Validator/ContentValidator.php
+++ src/Validator/ContentValidator.php
@@ -4,32 +4,27 @@ declare(strict_types=1);
 
 namespace SmartAssert\YamlFile\Validator;
 
-use SmartAssert\YamlFile\Exception\UnexpectedSubjectTypeException;
 use SmartAssert\YamlFile\Model\Validation\ContentContext;
 use SmartAssert\YamlFile\Model\Validation\Validation;
 use SmartAssert\YamlFile\Model\Validation\ValidationInterface;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Parser;
 
-class ContentValidator implements ValidatorInterface
+class ContentValidator
 {
     public function __construct(
         private Parser $yamlParser,
     ) {
     }
 
-    public function validate(string|object $subject): ValidationInterface
+    public function validate(string $content): ValidationInterface
     {
-        if (false === is_string($subject)) {
-            throw UnexpectedSubjectTypeException::create('string', $subject);
-        }
-
-        if ('' === trim($subject)) {
+        if ('' === trim($content)) {
             return Validation::createInvalid(ContentContext::NOT_EMPTY);
         }
 
         try {
-            $this->yamlParser->parse($subject);
+            $this->yamlParser->parse($content);
         } catch (ParseException $e) {
             return Validation::createInvalid(ContentContext::IS_YAML, $e->getMessage());
         }
