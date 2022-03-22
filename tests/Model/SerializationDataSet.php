9c44d75dcf9e2a3922c60234f86d2121e79d8ec3
Jon Cram <webignition@gmail.com>
2022-03-22 10:53:54 +0000

    De-duplicate test data

---
+++ tests/Model/SerializationDataSet.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\Tests\YamlFile\Model;
+
+use SmartAssert\YamlFile\Collection\ProviderInterface;
+
+class SerializationDataSet
+{
+    public function __construct(
+        public readonly ProviderInterface $provider,
+        public readonly string $serialized
+    ) {
+    }
+}
