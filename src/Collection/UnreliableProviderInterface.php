1f7132cc99f01b0b629c607b1e8b375d3aa7c106
Jon Cram <webignition@gmail.com>
2022-03-22 18:08:51 +0000

    Add UnreliableProviderInterface

--- src/Collection/UnreliableProviderInterface.php
+++ src/Collection/UnreliableProviderInterface.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace SmartAssert\YamlFile\Collection;
 
+use SmartAssert\YamlFile\Exception\ProvisionException;
 use SmartAssert\YamlFile\YamlFile;
 
-interface ProviderInterface
+interface UnreliableProviderInterface extends ProviderInterface
 {
     /**
+     * @throws ProvisionException
+     *
      * @return \Generator<YamlFile>
      */
     public function getYamlFiles(): \Generator;
